03949cc5fd4ce2f995c28bc4be92d155665d088f
Aurélien Hamy <aurelienhamy@gmail.com>
2019-08-10 12:50:10 +0200

    Made preproc functions

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,78 @@
+use std::fs::File;
 use std::io;
+use std::io::prelude::*;
+use std::process::exit;
+
+const FILENAME: &str = "tables.moon";
 
 fn main() {
-    println!("Input your instruction :");
-    let mut line = String::new();
-    io::stdin()
-        .read_line(&mut line)
-        .expect("Failed to read line");
-
-    line = line.replace(" ", "").replace("\n", "");
-    let line: Vec<&str> = line.split(':').collect();
-    if line.len() != 2 || line[0].len() == 0 || line[1].len() == 0 {
-        println!("Error : incorrect syntax");
-    } else {
-        let instruction = line[0];
-        println!("\ninstruction : {:?}", instruction);
-
-        let operands: Vec<&str> = line[1].split(",").collect();
-        print!("List of operands : ");
-        for operand in operands {
-            print!("{:?} ; ", operand);
+    // On récupère le contenu du fichier Moon
+    let content = match open_file(FILENAME) {
+        Ok(file) => file,
+        Err(error) => {
+            eprintln!("Error : file {} does not exists ({})", FILENAME, error);
+            exit(1)
         }
-        println!();
+    };
+
+    // On transforme le programme en un tableau de tuples
+    let program: Vec<&str> = content.split('\n').collect();
+
+    let mut line_number = 0;
+    for line in program {
+        line_number += 1;
+        let str_line: String = line.to_string();
+        let (ins, op1, op2) = get_transformed_line(str_line, line_number);
+        println!(
+            "Instruction : {i}\nOperand 1 : {o1}\nOperand 2 : {o2}",
+            i = ins,
+            o1 = op1,
+            o2 = op2
+        )
+    }
+}
+
+// Permet d'ouvrir un fichier donné
+fn open_file(file_name: &str) -> io::Result<String> {
+    let mut file = File::open(file_name)?;
+    let mut content = String::new();
+    file.read_to_string(&mut content)?;
+    Ok(content)
+}
+
+// Nous permet d'obtenir un tuple contenant instruction, opérande 1 et opérande 2 pour une ligne donnée (String)
+fn get_transformed_line(line: String, line_number: i32) -> (String, String, String) {
+    // On enlève les caractères en trop et on remplace les lignes nulles par nll:nll
+    let mut line = line.replace(" ", "").replace("\n", "");
+    if line.len() == 0 {
+        line = String::from("nll:nll");
+    }
+
+    // On split instruction / opérandes
+    let splitted: Vec<&str> = line.split(':').collect();
+
+    // On vérifie qu'il n'y ait pas trop de séparateurs
+    if splitted.len() != 2 || splitted[0].len() == 0 || splitted[1].len() == 0 {
+        eprintln!("Error : incorrect syntax line {lnb}", lnb = line_number);
+        exit(1);
+    } else {
+        // On distingue maintenant instruction et opérandes
+        let instruction: String = splitted[0].to_string();
+        let operands: Vec<&str> = splitted[1].split(",").collect();
+        // On vérifie qu'op1 existe
+        let operand1 = if operands[0].len() != 0 {
+            String::from(operands[0])
+        } else {
+            eprintln!("Error : incorrect syntax line {lnb}", lnb = line_number);
+            exit(1);
+        };
+        // On donne une valeur quomcumquest à op2
+        let operand2 = if operands.len() == 2 {
+            String::from(operands[1])
+        } else {
+            String::new()
+        };
+        // On retourne le tuple
+        (instruction, operand1, operand2)
     }
 }
---
+++ tables.moon
@@ -0,0 +1,24 @@
+var: a, int
+set: a, 0
+var: b, int
+set: b, 0
+flg: table1
+
+flg: table2
+
+mul: a, b
+prt: _res
+
+add: b, 1
+ret: b
+
+sub: 1, b
+jne: _res, table2
+
+set: b, 0
+
+add: a, 1
+ret: a
+
+sub: 11, a
+jne: _res, table1
